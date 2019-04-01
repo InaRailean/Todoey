4ba4d79fb55a6069283c41281fa9657b71b1d34c
Ina Railean <inarailean01@gmail.com>
2019-04-01 11:38:07 +0100

    Persisting Data using Core data.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -83,16 +83,14 @@ class TodoListViewController: UITableViewController {
     }
     
     
-//    func loadCategoryItems() {
-//
-//        itemCategory = predicate
-//        
-//    }
-//    
-    
-    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
-        let predicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
-        request.predicate = predicate
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        if let additionalPredicate = predicate {
+           request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
+        } else {
+            request.predicate = categoryPredicate
+        }
+        
         do {
             itemArray = try context.fetch(request)
         } catch {
@@ -107,11 +105,10 @@ extension TodoListViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         let request : NSFetchRequest<Item> = Item.fetchRequest()
         
-        request.predicate = NSPredicate(format : "title CONTAINS[cd] %@", searchBar.text!)
-        
+        let predicate = NSPredicate(format : "title CONTAINS[cd] %@", searchBar.text!)
         request.sortDescriptors  = [NSSortDescriptor(key: "title", ascending: true)]
         
-        loadItems(with: request)
+        loadItems(with: request, predicate: predicate)
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
